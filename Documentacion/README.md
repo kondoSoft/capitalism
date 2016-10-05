ff6b461534ba3ffd42bceb8adeacaa5b2472602b
Williams Rincon <wrp91@hotmail.com>
2016-10-04 19:13:27 -0500

    Add-documentation-and-components of game

---
+++ Documentacion/README.md
@@ -0,0 +1,10 @@
+## Capitalism
+
+1.- En el primer elmento (representado en el rectangulo superior) tenemos una cantidad que representa la cantidad actual de dinero que el usuario posee, esta cantidad se puede utilizar para obtener nuevas empresas.
+  Dependiendo de la cantidad de dinero que se tenga es el tipo de negocio que el usuario podra adquirir.
+
+2.- En el siguente modulo (el cual, en pantalla hay 3) El primer rectangulo representa el tiempo que demorara la transaccion. El rectangulo que sigue (abajo del tiempo de transaccion) representa la cantidad de empresas que vas a adaquirir y el precio que el usuario pagara por estas. A la derecha de este modulo se ubica el icono que representa que tipo de negocio es, y abajo de este se encuentra la cantidad de negocios de este mismo tipo tiene el usuario.
+
+3.- En el ultimo modulo (parte inferior de la pantalla) representa un nuevo negocio que se va adquirir, dentro de este modulo podremos encontrar un icono en la parte izquierda que representa el tipo de negocio que el usuario va adquirir, y del lado derecho tenemos un submodulo que contienen la descripcion del negocio y el precio este.
+
+![](../mockup/main.png)
--- src/App.js
+++ src/App.js
@@ -2,16 +2,103 @@ import React, { Component } from 'react';
 import './App.css';
 
 class App extends Component {
+  constructor(props){
+    super(props)
+    this.state = {
+      buyedStand:{
+        lemonade:{
+          revenue:10,
+          standPrice:1000,
+          quantityStand:1,
+          time:500,
+          name: 'lemonade'
+        },
+        newspaper:{
+          revenue:100,
+          standPrice:1000,
+          quantityStand:1,
+          time:1500,
+          name:'newspaper'
+        },
+        donutShop:{
+          revenue:150,
+          standPrice:1050,
+          quantityStand:1,
+          time:1000,
+          name:'donutShop'
+        },
+        pizzaShop:{
+          revenue:150,
+          standPrice:1050,
+          quantityStand:1,
+          time:1000,
+          name:'pizzaShop'
+        }
+      },
+      availableStand:{
+        bank:{
+          revenue:150,
+          standPrice:1050,
+          quantityStand:1,
+          time:1000,
+          name:'bank'
+        },
+        movieStudio:{
+          revenue:150,
+          standPrice:1050,
+          quantityStand:1,
+          time:1000,
+          name:'movieStudio'
+        },
+        oilCompany:{
+          revenue:150,
+          standPrice:1050,
+          quantityStand:1,
+          time:1000,
+          name:'oilCompany'
+        }
+      }
+    }
+  }
   render() {
     return (
       <div className="container">
-        <Bar time={1000}/>
+        <MainContainer buyedStand={this.state.buyedStand} availableStand={this.state.availableStand}/>
       </div>
     );
   }
 }
 
-var Bar = React.createClass({
+var MainContainer = React.createClass({
+
+  render: function(){
+    console.log(this.props.availableStand);
+    return(
+      <div>
+        <Capital monto='50000' />
+        <div className='row'>
+          <GameItem />
+        </div>
+      </div>
+    )
+  }
+})
+
+var GameItem = React.createClass({
+  render: function(){
+    return(
+      <div>
+        <div className='rowContainer'>
+          <BarProgress time={1000}/>
+          <Thumbnail />
+        </div>
+        <RevenueBuyStandContainer />
+      </div>
+    )
+  }
+})
+
+var BarProgress = React.createClass({
   getInitialState(){
     return({
       width: 0
@@ -34,14 +121,67 @@ var Bar = React.createClass({
   },
   render() {
     return(
-      <div>
+      <div className='progressBar'>
         <div id="myProgress">
           <div id="myBar" style={{width:this.state.width + '%'}}></div>
-          <div id="label">10%</div>
+          <div id="label">00:10s</div>
         </div>
         <button onClick={()=>{this.cargar(this.props.time)}}>Iniciar</button>
       </div>
 
+    )
+  }
+})
+
+var Capital = React.createClass({
+  render: function() {
+    return(
+      <div className='capital'>
+        <span>{this.props.monto}</span>
+      </div>
+    )
+  }
+})
+
+var Thumbnail = React.createClass({
+  render: function(){
+    return(
+      <div className="thumb">
+        <img src="http://placehold.it/50x50" role="presentation" />
+        <div>100</div>
+      </div>
+        )
+  }
+})
+
+var RevenueBuyStandContainer = React.createClass({
+  render: function(){
+    return(
+      <div className='rev'>
+        <BuyStand />
+        <Revenue />
+      </div>
+    )
+  }
+})
+
+var Revenue = React.createClass({
+  render:function(){
+    return(
+      <div className='revenue'>
+        <span>$1000.00</span>
+      </div>
+    )
+  }
+})
+
+var BuyStand = React.createClass({
+  render: function(){
+    return(
+      <div className="buystand">
+        <span>buy x1 </span>
+        <span> $100.00</span>
+      </div>
     )
   }
 })
